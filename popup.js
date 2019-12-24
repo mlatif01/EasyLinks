d76b7a61ad76ad81b096e8acf6b395a7f8f51903
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-12-24 14:11:18 +0000

    added functionality for removing icons

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 
 const newBtn = document.querySelector('.btn-new');
+const delBtn = document.querySelector('.btn-del');
+const para = document.querySelector('p');
 const modalIcons = document.querySelector('.modal-icons');
 const modalHeader = document.querySelector('.modal-header');
+const URL = "http://localhost:3000/links/";
 
 refreshList();
 
@@ -10,7 +13,7 @@ function refreshList() {
     getLinks().then(
         data => {
             data.forEach(element => {
-                addIcon(element.url, element.icon);
+                addIcon(element.url, element.icon, element.id);
             });
         },
         error => {
@@ -23,9 +26,17 @@ function toggleNewButton() {
     newBtn.style.display === "none" ? newBtn.style.display = "block" : newBtn.style.display = "none";
 }
 
+function toggleDelButton() {
+    delBtn.style.display === "none" ? delBtn.style.display = "block" : delBtn.style.display = "none";
+}
+
 function createInputBoxes() {
-    // hide button
+    //hide del button
+    toggleDelButton();
+    // hide new button
     toggleNewButton();
+    // hide para
+    para.hidden = true;
     // create input boxes
     // first create div
     const txtNewInputBox = document.createElement('div');
@@ -47,6 +58,41 @@ function createInputBoxes() {
     window.addBtn.addEventListener('click', addItem);
 }
 
+function createDelOptions() {
+    // hide new button
+    toggleNewButton();
+    // hide delete button
+    toggleDelButton();
+    // hide para
+    para.hidden = true;
+    // create del options
+    // create paragraph and first create div
+    const newDiv = document.createElement('div');
+    // add content (a new input box) of the element - improve the below implementation
+    newDiv.innerHTML = `<p>Choose icon to delete</p>`;
+    // finally put it where it needs to appear
+    document.getElementById("newElementId").appendChild(newDiv);
+
+    // change all icons to red so that when they are clicked they can be deleted
+    // get all elements with class of flex
+    const flexElems = document.querySelectorAll('.flex');
+    // change all their colours to red to signify they can be deleted
+    flexElems.forEach( (element) => {
+        let div = element;
+        let divId = div.id;
+        let a = element.firstChild;
+        let i = a.firstChild;
+        div.removeChild;
+        div.appendChild(i);
+        // colour in each i element
+        i.style = "color: red";
+        // add event listener to icons for deleting and pass in divId as param
+        div.addEventListener('click', () => {
+            deleteItem(divId);
+        });
+    });
+};
+
 function resetModalContent() {
     // if new button not showing, reset modal content when bottom of screen is clicked (modal-icons div)
     if (newBtn.style.display === "none") {
@@ -56,49 +102,100 @@ function resetModalContent() {
             e.removeChild(child);
             child = e.lastElementChild;
         }
+        para.hidden = false;
     }
     newBtn.style.display = "block";
+    // -------------------------------------------------------
+    // delete button reset
+    if (delBtn.style.display === "none") {
+        const e = document.getElementById("newElementId");
+        let child = e.lastElementChild;
+        while (child) {
+            e.removeChild(child);
+            child = e.lastElementChild;
+        }
+        para.hidden = false;
+
+        removeIcons();
+        // refresh list of icons
+        refreshList();
+    }
+    delBtn.style.display = "block";
 }
 
-function addIcon(url, icon) {
+function removeIcons() {
+    // get all elements with class of flex
+    const flexElems = document.querySelectorAll('.flex');
+    // remove all icons and then refresh List
+    flexElems.forEach( (element) => {
+        let div = element;
+        div.remove();
+    });
+}
+
+function addIcon(url, icon, id) {
     // get string values of the url and icon
     const urlVal = url;
     const iconVal = icon;
-    // create div with id of flex
+    const idVal = id;
+    // create div with id of flex 
     const newIconDiv = document.createElement('div');
     newIconDiv.className = 'flex';
+    newIconDiv.id = id;
     // add url value and icon value
     newIconDiv.innerHTML = `<a href='${urlVal}' target='_blank'><i class='${getIcon(iconVal)}'></i></a>`;
-    console.log(newIconDiv);
     // finally put it where it needs to appear in the flex container div
     document.querySelector('.flex-container').appendChild(newIconDiv);
     // reset modal content after adding item
     resetModalContent();
 }
 
-// this function gets the url and icon value and posts it to the db.json
+// this function gets the url and icon value and calls postLink to call http post request to db
 function addItem() {
     const urlBox = document.getElementById('inputBox');
     const iconBox = document.getElementById('selectBox');
     const urlVal = urlBox.value;
     const iconVal = iconBox.value;
     postLink(urlVal, iconVal);
+    removeIcons();
+    resetModalContent();
+}
+
+// gets the id for the item to be deleted then calls deleteLink to send http delete request to db with id passed as param
+function deleteItem(divId) {
+    if (confirm('Are you sure you want to delete icon with ID: ' + divId)) {
+        deleteLink(divId);
+    } else {
+        // do nothing
+    }
+    resetModalContent();
 }
 
 function postLink(urlVal, iconVal) {
         // post to db
         let link = {
-            id: urlVal,
+            url: urlVal,
             icon: iconVal
         };
     
         let promise = $.post(
-            "http://localhost:3000/links", link
+            URL, link
         );
         promise.then(
             data => console.log('data: ', data),
             error => console.log('error: ', error)
         );
+}
+
+function deleteLink(id) {
+    // delete from db
+    let promise = $.delete(
+        URL + id,
+    );
+    promise.then(
+        data => console.log('Deleted successfully'),
+        error => console.log('error: ', error)
+    );
 }
 
 // takes in an icon value and returns the appropriate font awesome icon class
@@ -122,7 +219,10 @@ function getLinks() {
         "http://localhost:3000/links"
     );
     promise.then(
-        data => console.log('data: ', data),
+        data => {
+            console.log('data: ', data);
+
+        },
         error => console.log('error: ', error)
     );
     return promise;
@@ -130,5 +230,23 @@ function getLinks() {
 
 // Event Listeners
 newBtn.addEventListener('click', createInputBoxes);
-modalIcons.addEventListener('click', resetModalContent);
+delBtn.addEventListener('click', createDelOptions);
 modalHeader.addEventListener('click', resetModalContent);
+
+// extend $ to use delete
+$.delete = function(url, data, callback, type){
+
+    if ( $.isFunction(data) ){
+      type = type || callback,
+      callback = data,
+      data = {}
+    }
+  
+    return $.ajax({
+      url: url,
+      type: 'DELETE',
+      success: callback,
+      data: data,
+      contentType: type
+    });
+  }
